c15f66cf35b5fe4cf9f3b9110ae5fd3365cb22b8
Pair <pair@pivotal.io>
2016-03-29 17:55:21 +0800

    Initialize Project

---
+++ src/main/java/io/pivotal/singapore/MilliwaysApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.singapore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilliwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilliwaysApplication.class, args);
+	}
+}
