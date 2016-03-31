05159fda8dfc1520d7cbb04b2457d73752587236
Swayam Narain + Alan Yeo <pair+sn+ay@pivotal.io>
2016-03-31 16:34:18 +0800

    Setup forked process of Marvin application when running integration tests
    [#116499277]

--- src/test/java/io/pivotal/singapore/integration/MarvinIntegrationTest.java
+++ src/test/java/io/pivotal/singapore/integration/MarvinIntegrationTest.java
@@ -5,16 +5,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MilliwaysApplication.class)
 @WebAppConfiguration
+@IntegrationTest
 public class MarvinIntegrationTest {
 
     @Value("${api.marvin.register}")
