a51135e8a755264097926e998ad82d5363a5fabd
Pair <pair@pivotal.io>
2016-04-08 15:35:35 +0800

    Use github release for fetching of artifacts

--- ci/tasks/integration.sh
+++ ci/tasks/integration.sh
@@ -5,7 +5,7 @@ gosu postgres pg_ctl -D $PGDATA start
 
 mkdir milliways-source/lib
 
-cp -v marvin-source/build/libs/marvin-*.jar milliways-source/lib/
+cp -v marvin-release/*.jar milliways-source/lib/
 
 cd milliways-source
 
