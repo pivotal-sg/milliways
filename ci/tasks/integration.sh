3970657ba233d62e0a5ff5f7e5f1b2f0c5d0e412
Pair <pair@pivotal.io>
2016-04-08 15:08:40 +0800

    Copy over marvin fat jar from marvin-source

--- ci/tasks/integration.sh
+++ ci/tasks/integration.sh
@@ -5,10 +5,9 @@ gosu postgres pg_ctl -D $PGDATA start
 
 mkdir milliways-source/lib
 
-cp -v marvin-build/marvin-*.jar milliways-source/lib/
+cp -v marvin-source/build/libs/marvin-*.jar milliways-source/lib/
 
 cd milliways-source
 
 ./gradlew --info test &&
-    ./gradlew assemble &&
-    cp -v build/libs/milliways-*.jar ../milliways-build/
+    ./gradlew assemble
