f83d03a75143a9309367b2aef557511142b1b39c
Pair <pair@pivotal.io>
2016-04-11 12:27:23 +0800

    Configure milliways release

--- ci/tasks/integration.sh
+++ ci/tasks/integration.sh
@@ -10,4 +10,7 @@ cp -v marvin-release/*.jar milliways-source/lib/
 cd milliways-source
 
 ./gradlew --info test &&
-    ./gradlew assemble
+    ./gradlew assemble &&
+    cp -v build/libs/milliways-*.jar ../milliways-release/ &&
+    cp -v manifest.yml ../milliways-release/ &&
+    date "+%Y%m%d%H%M%S" > ../milliways-release/release-tag
